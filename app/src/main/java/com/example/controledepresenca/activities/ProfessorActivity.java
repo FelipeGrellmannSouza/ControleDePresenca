44b657acd938c9c04886a2cef9be3f219104b170
FelipeGrellmannSouza <felipegrellmann1@gmail.com>
2023-11-04 00:36:36 -0300

    Tela Professor e Aluno

--- app/src/main/java/com/example/controledepresenca/activities/ProfessorActivity.java
+++ app/src/main/java/com/example/controledepresenca/activities/ProfessorActivity.java
@@ -2,15 +2,28 @@ package com.example.controledepresenca.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.example.controledepresenca.DAO.UsuarioDAO;
 import com.example.controledepresenca.R;
 
 public class ProfessorActivity extends AppCompatActivity {
-
+    UsuarioDAO usuarioDAO;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_professor);
+
+        usuarioDAO = new UsuarioDAO(this);
+
+    }
+
+    public void deslogarProfessor(View view){
+        usuarioDAO.deslogar();
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
